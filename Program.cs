42089a59f1d62aadce1ceb016cb5c2d430374218
igorzverlov <izverlov@gamil.com>
2022-02-21 02:59:10 +0300

    program.cs updated

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+string[] stringArray = { "hello", "2", "world", ":-)", "88", "-2" };
+
+void PrintStringArray(string[] stringArray)
+{
+  Write("["); 
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    Write($"\"{stringArray[i]}\"");
+    if (i != stringArray.Length - 1)
+    {
+      Write(", ");
+    }
+  }
+  Write("]");
+}
+
+int SearchStringLessOrEqual3Symbols(string[] stringArray)
+{
+  int numberOfString = 0;
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    if (stringArray[i].Length < 4)
+    {
+      numberOfString++;
+    }
+  }
+  return numberOfString;
+}
+
+string[] CreateArrayLessOrEqual3Symbols(string[] stringArray, int sizeNewArray)
+{
+  string[] newStringArray = new string[sizeNewArray];
+  int j = 0;
+  for (int i = 0; i < stringArray.Length; i++)
+  {
+    if (stringArray[i].Length < 4)
+    {
+      newStringArray[j] = stringArray[i];
+      j++; 
+    }
+  }
+  return newStringArray;
+}
+
+
+PrintStringArray(stringArray);
+int sizeNewArray = SearchStringLessOrEqual3Symbols(stringArray); 
+string[] newStringArray = CreateArrayLessOrEqual3Symbols(stringArray, sizeNewArray); 
+Write(" --> ");
+PrintStringArray(newStringArray);
